8c9489364a4880f8db97dc8bd4ce37b6b3a02eef
codyowl <codyowl@gmail.com>
2016-07-23 20:11:36 +0530

    Model.py added

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from mongoengine import *
 
-# Create your models here.
+class Choice(Document):
+	choice_text = StringField(max_length=200)
+	votes = IntField(default=0)
+
+	def __unicode__(self):
+		return self.choice_text
+
+
+class Poll(Document):
+	question = StringField(max_length=200)
+	pub_date = DateTimeField(help_text='date published')
+	choices = ListField(ReferenceField(Choice))
+
+	def __unicode__(self):
+		return self.question
+
+
